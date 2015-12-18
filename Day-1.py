d39c6c332eae2dc985479a9133b7fbab4e6d109f
Eyacek <ye351190@yahoo.com>
2015-12-17 18:57:34 -0600

    Update Day-1.py

--- Day-1.py
+++ Day-1.py
@@ -2,13 +2,7 @@
 
 #  Description: Returns the day of the week when given year, month, and date.
 
-#  Student Name: Evan Yacek
-
-#  Student UT EID: ety78
-
-#  Course Name: CS 303E
-
-#  Unique Number: 50475
+#  Evan Yacek
 
 #  Date: 9/12/2015
 
